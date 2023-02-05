8e362e82f1a988a305346f29694b299d45a8c255
Josh <josh@mail.com>
2023-02-05 23:57:51 +0000

    updated main

--- Jenkins_Server/main.tf
+++ Jenkins_Server/main.tf
@@ -17,6 +17,42 @@ data "aws_vpc" "default" {
   cidr_block = "172.31.0.0/16"
 }
 
+
+resource "aws_security_group" "jenkins_security_group" {
+  name        = "jenkins_security_group"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress{
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
 
@@ -40,14 +76,18 @@ data "aws_ami" "amazon-linux-2" {
 resource "aws_instance" "jenkins_server" {
   ami           = data.aws_ami.amazon-linux-2.id
   instance_type = "t2.micro"
-  vpc_id        = aws_vpc.default.id
+  vpc_security_group_ids = [aws_security_group.jenkins_security_group.id]
   user_data = <<EOF
-          #!/bin/bash
-          sudo yum update
-          sudo yum install httpd -y
-          sudo systemctl start httpd
-          sudo systemctl enable httpd
-  EOF
+#!/bin/bash
+yum update –y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum upgrade
+amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+EOF
     
   tags = {
     Name = "JenkinsServer"
