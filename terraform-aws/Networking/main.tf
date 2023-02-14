1c8626bf9a73db7782b2ac03fedcef3e5d587ddf
Josh <josh@mail.com>
2023-02-14 09:11:16 +0000

    edited main.tf and variables.tf

--- terraform-aws/Networking/main.tf
+++ terraform-aws/Networking/main.tf
@@ -13,3 +13,15 @@ resource "aws_vpc" "luit_vpc" {
         Name = "luit_vpc-${random_integer.random.id}"
     }
 }
+
+resource "aws_subnet" "luit_public_subnet" {
+    count = length(var.public_cidrs)
+    vpc_id = aws_vpc.luit_vpc.id
+    cidr_block = var.public_cidrs[count.index]
+    map_public_ip_on_launch = true
+    availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"][count.index]
+    
+    tags = {
+        Name = "luit_public_subnet_${count.index + 1}"
+    }
+}
