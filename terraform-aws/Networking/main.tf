715fbfba128d45b4fc5a6dce6b5dec0a4c183181
Josh <josh@mail.com>
2023-02-16 22:20:47 +0000

    changes to main.tfs variables.tf

--- terraform-aws/Networking/main.tf
+++ terraform-aws/Networking/main.tf
@@ -1,4 +1,8 @@
 # --- networking/main.tf ---
+
+data "aws_availability_zones" "available" {}
+
+
 resource "random_integer" "random" {
     min = 1
     max = 100
@@ -15,11 +19,11 @@ resource "aws_vpc" "luit_vpc" {
 }
 
 resource "aws_subnet" "luit_public_subnet" {
-    count = length(var.public_cidrs)
+    count = var.public_sn_count
     vpc_id = aws_vpc.luit_vpc.id
     cidr_block = var.public_cidrs[count.index]
     map_public_ip_on_launch = true
-    availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"][count.index]
+    availability_zone = data.aws_availability_zones.available.names[count.index]
     
     tags = {
         Name = "luit_public_subnet_${count.index + 1}"
@@ -27,13 +31,44 @@ resource "aws_subnet" "luit_public_subnet" {
 }
 
 resource "aws_subnet" "luit_private_subnet" {
-    count = length(var.private_cidrs)
+    count = var.private_sn_count
     vpc_id = aws_vpc.luit_vpc.id
     cidr_block = var.private_cidrs[count.index]
     map_public_ip_on_launch = false
-    availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"][count.index]
+    availability_zone = data.aws_availability_zones.available.names[count.index]
     
     tags = {
         Name = "luit_private_subnet_${count.index + 1}"
     }
 }
+
+resource "aws_internet_gateway" "luit_internet_gateway" {
+    vpc_id = aws_vpc.luit_vpc.id
+    
+    tags = {
+        Name = "luit_igw"
+    }
+}
+
+resource "aws_route_table" "luit_public_rt" {
+    vpc_id = aws_vpc.luit_vpc.id
+    
+    tags = {
+        Name = "luit_public"
+    }
+}
+
+resource "aws_route" "default_route" {
+    route_table_id = aws_route_table.luit_public_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.luit_internet_gateway.id
+}
+
+resource "aws_default_route_rable" "luit_private_rt" {
+    default_route_table_id = aws_vpc.luit_vpc.default_route_table_id
+    
+    tags = {
+        name = "luit_private"
+    }
+}
+
--- terraform-aws/Networking/variables.tf
+++ terraform-aws/Networking/variables.tf
@@ -12,3 +12,10 @@ variable "private_cidrs" {
     type = list
 }
 
+variable "private_sn_count" {
+    type = number
+}
+
+variable "public_sn_count" {
+    type = number
+}
